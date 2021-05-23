565764511cd87c8e4f14680309e82b79794f68f8
jkinathan <jkinobe@gmail.com>
2021-05-23 11:42:48 +0300

    update proc3

--- THENETCONF/THENETCONF/settings.py
+++ THENETCONF/THENETCONF/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k7k$y!xu#2)-w*_vnqx6!*mbg(uyflocxuma+n@6add%867)16'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','thenetconf.herokuapp.com']
 
