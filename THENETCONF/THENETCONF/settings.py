513933a1f244b8776e41c4c50864f9ff72eb3850
jkinathan <jkinobe@gmail.com>
2021-08-11 18:10:15 +0300

    fixed images and also dockerized project

--- THENETCONF/THENETCONF/settings.py
+++ THENETCONF/THENETCONF/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
+MEDIA_URL = '/static/images/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
